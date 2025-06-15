f58b88e5ebd1382590447744e6ff706f09114a86
gnklakshan <132909147+gnklakshan@users.noreply.github.com>
2025-06-15 10:20:25 +0530

    transaction page created

---
+++ app/(main)/transaction/_component/AddTransactionForm.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+import React from "react";
+
+const AddTransactionForm = ({ accounts, categories }) => {
+  return <div></div>;
+};
+
+export default AddTransactionForm;
---
+++ app/(main)/transaction/create/page.jsx
@@ -0,0 +1,17 @@
+import { getUserAccounts } from "@/action/dashbord";
+import { defaultCategories } from "@/Data/category";
+import React from "react";
+import AddTransactionForm from "../_component/AddTransactionForm";
+
+const AddTransactionPage = async () => {
+  const accounts = await getUserAccounts();
+
+  return (
+    <div className="max-w-3xl mx-auto px-5">
+      <h1 className="text-5xl gradient-title mb-8">Add Transaction</h1>
+      <AddTransactionForm accounts={accounts} categories={defaultCategories} />
+    </div>
+  );
+};
+
+export default AddTransactionPage;
